5b7c31adbeb8984fd21da4ba625221b3056037fa
GermanSTr <gershev1994@gmail.com>
2024-02-13 17:59:12 +0200

    baseTypes

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,33 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
+type details = {
+  createAt: Date;
+  updateAt: Date;
+};
+type user = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: details;
+};
+const page1: user = {
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page2: user = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
